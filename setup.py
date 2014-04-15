d8685c3d1e3267def2df8d7ee1c9b6ee7e01fe47
Dean Gardiner <gardiner91@gmail.com>
2014-04-15 16:51:13 +1200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='PyEmitter',
+    version='1.0.0',
+    url='http://github.com/fuzeman/PyEmitter/',
+
+    author='Dean Gardiner',
+    author_email='me@dgardiner.net',
+
+    packages={'': 'pyemitter'},
+    platforms='any',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+)
