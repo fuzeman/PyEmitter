c83d57d416a2ea94945133e393ee0c989a4a8cac
Dean Gardiner <gardiner91@gmail.com>
2014-05-28 01:14:29 +1200

    Bumped version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyEmitter',
-    version='1.1.0',
+    version='1.1.1',
     url='http://github.com/fuzeman/PyEmitter/',
 
     author='Dean Gardiner',
